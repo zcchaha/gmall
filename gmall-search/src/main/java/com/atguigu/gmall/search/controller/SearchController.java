8327032c6725dc4ce56c99a73a379d3586f4a0bf
zhangchuang <1730070376@qq.com>
2020-05-29 21:32:54 +0800

    day12-模板引擎(Thymeleaf)及页面渲染

--- gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -5,6 +5,8 @@ import com.atguigu.gmall.search.bean.SearchParam;
 import com.atguigu.gmall.search.bean.SearchResponseVo;
 import com.atguigu.gmall.search.service.SeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ZCC
  * @date 2020/5/27 18:03
  */
-@RestController
+@Controller
 public class SearchController {
 
     @Autowired
     private SeacherService seacherService;
 
     @GetMapping("search")
-    public ResponseVo<Object> search(SearchParam searchParam){
+    public String search(SearchParam searchParam, Model model){
         SearchResponseVo searchResponseVo = this.seacherService.search(searchParam);
-        return ResponseVo.ok(searchResponseVo);
+        model.addAttribute("response",searchResponseVo);
+        model.addAttribute("searchParam",searchParam);
+
+        return "search";
     }
 
 }
